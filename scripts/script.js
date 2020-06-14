56fc58677aa8e610a89eb2cbd9aa87d96fe2fc12
mityaii1 <63926278+mityaii1@users.noreply.github.com>
2020-06-14 17:41:46 +0500

    Исправить замечания

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
+// Переменные profile
 let profile = document.querySelector('.profile');
 let profileEditButton = profile.querySelector('.profile__edit-button');
 let profileTitle = profile.querySelector('.profile__title');
 let profileSubtitle = profile.querySelector('.profile__subtitle');
+
+// Переменные popup
 let popup = document.querySelector('.popup');
 let popupContainer = popup.querySelector('.popup__container');
 let popupCloseIcon = popup.querySelector('.popup__close-icon');
@@ -9,20 +12,27 @@ let popupButtonSave = popup.querySelector('.popup__button-save');
 let newName = popup.querySelector('.popup__input_name');
 let newAboutMe = popup.querySelector('.popup__input_about-me');
 
+// Открыть popup и заполнить значения из profile
 function openPopup() {
+    newName.value = profileTitle.textContent;
+    newAboutMe.value = profileSubtitle.textContent;
     popup.classList.add('popup_opened');
+    
 };
+
+// Закрыть popup
 function closePopup() {
     popup.classList.remove('popup_opened');
 };
+
+// Изменить значения profile из формы popup
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileTitle.textContent = newName.value;
     profileSubtitle.textContent = newAboutMe.value;
-    return false;
+    closePopup();
 }; 
 
 profileEditButton.addEventListener('click', openPopup);
 popupCloseIcon.addEventListener('click', closePopup);
-popupButtonSave.addEventListener('click', closePopup);
 popupContainer.addEventListener('submit', formSubmitHandler);
